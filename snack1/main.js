68bef9c32e438770c8d0fddb278e8ec24bd2395b
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-28 11:07:02 +0200

    snack1

--- snack1/main.js
+++ snack1/main.js
@@ -3,14 +3,14 @@
 L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
 
-let number1 = prompt(`Scrivi un numero`)
+let number1 = prompt(`Scrivi un numero`);
 
-let number2 = prompt(`Scrivi un'altro numero`)
+let number2 = prompt(`Scrivi un'altro numero`);
 
 if(number1>number2){
-    document.writeln(`<p>${number1}</p>`)
+    document.writeln(`<p>${number1} è maggiore di ${number2}</p>`);
 } else if(number1<number2){
-    document.writeln(`<p>${number2}</p>`)
+    document.writeln(`<p>${number2} è maggiore di ${number1}</p>`);
 } else{
-    document.writeln(`<p>${number1}</p>`)
+    document.writeln(`<p>${number1} è uguale a ${number2}</p>`);
 }
